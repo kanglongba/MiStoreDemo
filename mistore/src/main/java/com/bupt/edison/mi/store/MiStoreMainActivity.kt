f8ff6493d9a239f698f4ce71a203831b201e6c7d
edison <edison.qian@applovin.com>
2023-11-14 14:46:27 +0800

    添加mi和oppo

---
+++ mistore/src/main/java/com/bupt/edison/mi/store/MiStoreMainActivity.kt
@@ -0,0 +1,11 @@
+package com.bupt.edison.mi.store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MiStoreMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mi_store_main)
+    }
+}
---
+++ oppostore/src/main/java/com/bupt/edison/oppo/store/OppoStoreMainActivity.kt
@@ -0,0 +1,11 @@
+package com.bupt.edison.oppo.store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OppoStoreMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oppo_store_main)
+    }
+}
