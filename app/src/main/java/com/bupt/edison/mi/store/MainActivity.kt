142c2e0259425c9dc7e6f945b4f918d340ab72b1
edison <edison.qian@applovin.com>
2023-11-14 14:59:02 +0800

    修改包名

--- app/src/main/java/com/bupt/edison/mi/store/MainActivity.kt
+++ app/src/main/java/com/bupt/edison/mi/store/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bupt.edison.mi.store
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bupt.edison.app.store.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
